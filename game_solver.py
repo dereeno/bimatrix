0a5df9df977db6292c65fb95221c516de72aedff
Dan Aharoni <aharonidan@gmail.com>
2016-08-19 12:14:23 +0100

    last try

--- game_solver.py
+++ game_solver.py
@@ -2,8 +2,10 @@ import os
 import index
 
 def run():
+    os.system('gcc -O3 -o lrsnash lrsnash.c lrsnashlib.c lrslib.c lrsmp.c')
     os.system('./lrsnash lrsnash_input > lrsnash_output')
     execfile('process_lrsnash_output.py')
+    os.system('gcc -O3 -o clique  coclique3.c')
     os.system('./clique < clique_input > clique_output')
     index.main()
 
