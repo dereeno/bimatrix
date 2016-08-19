946cc41bdc8b3b7963389a432074e84bbb5085e5
Dan Aharoni <aharonidan@gmail.com>
2016-08-19 13:13:09 +0100

    no message

--- game_solver.py
+++ game_solver.py
@@ -2,10 +2,12 @@ import os
 import index
 
 def run():
-    os.system('gcc -O3 -o lrsnash lrsnash.c lrsnashlib.c lrslib.c lrsmp.c')
+    if not os.path.isfile('lrsnash'): #ugly hack to make heroku compile lrsnash
+        os.system('gcc -O3 -o lrsnash lrsnash.c lrsnashlib.c lrslib.c lrsmp.c')
     os.system('./lrsnash lrsnash_input > lrsnash_output')
     execfile('process_lrsnash_output.py')
-    os.system('gcc -O3 -o clique  coclique3.c')
+    if not os.path.isfile('clique'): #ugly hack to make heroku compile clique
+        os.system('gcc -O3 -o clique  coclique3.c')
     os.system('./clique < clique_input > clique_output')
     index.main()
 
---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+gcc -O3 -o lrsnash lrsnash.c lrsnashlib.c lrslib.c lrsmp.c
+gcc -O3 -o clique  coclique3.c
+
+
