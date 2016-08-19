783386469166598795f4c4c2df944553c7c7d735
Dan Aharoni <aharonidan@gmail.com>
2016-08-19 10:21:32 +0100

    trying fool heroic

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn app:app
+web: ./lrsnash lrsnash_input > lrsnash_output
+web: ./clique < clique_input > clique_output
--- game_solver.py
+++ game_solver.py
@@ -2,8 +2,8 @@ import os
 import index
 
 def run():
-    os.system('lrsnash lrsnash_input > lrsnash_output')
+    os.system('./lrsnash lrsnash_input > lrsnash_output')
     execfile('process_lrsnash_output.py')
-    os.system('clique < clique_input > clique_output')
+    os.system('./clique < clique_input > clique_output')
     index.main()
 
